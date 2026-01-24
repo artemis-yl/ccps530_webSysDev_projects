3a153a654a245e435fa3e38f5acdee791ede66a3
ArtemisLee <yulim.lee@torontomu.ca>
2026-01-24 13:08:15 -0500

    lecture1

--- lab2/lab2_technicalReport.md
+++
@@ -1,5 +0,0 @@
-
-
-![Alt text](lab2/lab2_fullWebPage_Screenshot.png "Screen of my validator result")
-
-![Alt text](lab2/lab2_html5Validator_screenshot.png "Screen of my validator result")
---
+++ lectureNotes/lecture2_script.js
@@ -0,0 +1,11 @@
+function myFunctionExt() {
+    document.getElementById("demo2").innerHTML = "Paragraph changed via external!.";
+}
+
+function myFunctionMath() {
+    var x, y, z;
+    x = 5;
+    y = 11;
+    z = x + y;
+    document.getElementById("demoMath").innerHTML = z;
+}
